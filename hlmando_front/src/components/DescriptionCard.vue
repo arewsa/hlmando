0b9275760de5268e5999adacfd7f0ad0027a001f
arewsa <arewsa5@gmail.com>
2025-07-05 15:19:00 +0300

    all front finished

--- hlmando_front/src/components/DescriptionCard.vue
+++ hlmando_front/src/components/DescriptionCard.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="w-full flex mt-20 border-2 border-gray-300 rounded-2xl p-10">
     <div class="flex flex-col items-center justify-center ml-10 mr-20">
-      <i :class="icon" style="font-size: 8rem; margin-bottom: 1rem"></i>
-      <h1 class="text-black text-3xl text-center">{{ title }}</h1>
+      <i :class="icon" class="icon-responsive" style="margin-bottom: 1rem"></i>
+      <h1 class="text-black 2xl:text-3xl xl:text-2xl text-xl text-center">{{ title }}</h1>
       <div class="bg-yellow-1 w-full h-1 mt-5"></div>
     </div>
     <div class="flex flex-col gap-2 w-full">
-      <div v-for="item in description" :key="item" class="relative pl-6 pb-4">
+      <div v-for="item in description" :key="item" class="relative pl-6 2xl:pb-4 pb-2">
         <i
           class="pi pi-circle-fill absolute"
           style="font-size: 0.5rem; left: 0; top: 0.75rem"
         ></i>
-        <p class="text-black text-3xl">{{ item }}</p>
+        <p class="text-black 2xl:text-2xl xl:text-xl text-lg">{{ item }}</p>
       </div>
     </div>
   </div>
---
+++ hlmando_front/src/components/DescriptionCardSmall.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="border-2 border-gray-300 rounded-2xl py-5 px-2 flex flex-col gap-5 items-center justify-center h-full">
+      <div class="flex flex-col items-center justify-center">
+        <i :class="icon" class="icon-responsive" style="margin-bottom: 1rem"></i>
+        <h1 class="text-black text-xl sm:text-2xl text-center">{{ title }}</h1>
+      </div>
+      <div class="flex flex-col gap-2 w-full">
+        <div v-for="item in description" :key="item" class="pb-2 px-4 flex items-center gap-2">
+          <i class="pi pi-circle-fill pr-2" style="font-size: 0.5rem"></i>
+          <p class="text-black text-sm sm:text-base break-words">{{ item }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  defineProps<{
+    title: string
+    description: string[]
+    icon: string
+  }>()
+  </script>
+  
+  <style scoped></style>
+  
